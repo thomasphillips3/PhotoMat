637ad044fead714dd4275795247b85c8b3c0e1cb
Thomas Phillips <thomasphillips3@gmail.com>
2019-06-01 13:39:04 -0700

    adds user sign-up functionality

--- app/src/main/java/com/aspire/photomat/SignUpActivity.kt
+++ app/src/main/java/com/aspire/photomat/SignUpActivity.kt
@@ -4,12 +4,28 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.parse.ParseUser
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View?) {
         when(view?.getId()) {
-            R.id.buttonSignUp -> println(R.string.sign_up)
+            R.id.buttonSignUp -> {
+                val appUser = ParseUser()
+                appUser.email = textEmail.text.toString()
+                appUser.username = textUsername.text.toString()
+                appUser.setPassword(textPassword.text.toString())
+
+                appUser.signUpInBackground { e ->
+                    if (e == null) {
+                        Toast.makeText(this, "success", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this, e.message.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
             R.id.buttonLogin -> println(R.string.log_in)
         }
     }
